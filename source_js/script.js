320269e81f36d7c8dd0f2c4f4b7969a76c21ead3
Johnny Chang <johnny@johnnychang.com>
2016-02-29 15:22:11 -0600

    Monster commit. Added carousel, responsive navigation bar, video, footer, animations for splash page. Changed particle load times.

--- source_js/script.js
+++ source_js/script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
     dotColor: '#bdc3c7',
     lineColor: '#ecf0f1',
     density: 20000,
-    particleRadius: 5,
+    particleRadius: 3,
     proximity: 50,
     lineWidth: 0.5,
     // curvedLines: true,
@@ -17,3 +17,28 @@ document.addEventListener('DOMContentLoaded', function () {
   // var splash = document.getElementById('splash');
   // splash.style.marginTop = - splash.offsetHeight + 'px';
 }, false);
+
+// Start Orbit carousel.
+// var prize_orbit_target = $("#prize_orbit_target");
+// var prize_orbit = new Foundation.Orbit(prize_orbit_target, options);
+
+// Start Owl Carousel.
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    "items": 1,
+    "navigation": true,
+    "responsive": true,
+    "autoPlay": true
+  });
+
+  $(document).on("scroll", function() {
+    if ($(document).scrollTop() > 80) {
+      console.log("Scroll event detected.");
+      $(".title-bar").removeClass("large").addClass("small");
+      $(".top-bar").removeClass("large").addClass("small");
+    } else {
+      $(".title-bar").removeClass("small").addClass("large");
+      $(".top-bar").removeClass("small").addClass("large");
+    }
+  });
+});
