e07ea3a94676d00f26bd73eb23417f3428e89a90
Johnny Chang <johnny@johnnychang.com>
2016-02-28 19:39:27 -0600

    Added particleground effect and selected background pictures.

--- source_js/script.js
+++ source_js/script.js
@@ -1,1 +1,14 @@
 // Write any custom javascript functions here
+
+// For particle background
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa',
+    density: 40000,
+    // curvedLines: true,
+    parallaxMultiplier: 4
+  });
+  // var splash = document.getElementById('splash');
+  // splash.style.marginTop = - splash.offsetHeight + 'px';
+}, false);
